4c18e33e67bd9661912d5cec542f193c320910db
Yannick <yannick.herrero@acelys.fr>
2025-07-21 09:46:38 +0200

    remove copilot and avante, remove copilot suggestions from cmp list

--- config/linux/dotconfig/nvim/lua/plugins/autocomplete.lua
+++ config/linux/dotconfig/nvim/lua/plugins/autocomplete.lua
@@ -31,10 +31,9 @@ return {
           documentation = cmp.config.window.bordered(),
         },
         sources = cmp.config.sources({
-          { name = "copilot", group_index = 2 },
           { name = "nvim_lsp", group_index = 2 },
-          { name = "path", group_index = 2 },
-          { name = "luasnip", group_index = 2 },
+          { name = "path",     group_index = 2 },
+          { name = "luasnip",  group_index = 2 },
         }, {
           { name = "buffer" },
         }),
--- config/linux/dotconfig/nvim/lua/plugins/copilot.lua
+++ config/linux/dotconfig/nvim/lua/plugins/copilot.lua
@@ -1,22 +1,5 @@
 return {
   {
-    "zbirenbaum/copilot-cmp",
-    event = "InsertEnter",
-    config = function()
-      require("copilot_cmp").setup()
-    end,
-    dependencies = {
-      "zbirenbaum/copilot.lua",
-      cmd = "Copilot",
-      config = function()
-        require("copilot").setup({
-          suggestion = { enabled = false },
-          panel = { enabled = false },
-        })
-      end,
-    },
-  },
-  {
     "supermaven-inc/supermaven-nvim",
     config = function()
       require("supermaven-nvim").setup({
